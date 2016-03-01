ee8e528944818937c285a7a6c534b049364c7511
Donglin Song <songdl2014@foxmail.com>
2016-03-01 19:43:12 +0800

    添加HTML模板，重构login.aspx

--- ERP/Web/MasterPage.master.cs
+++ ERP/Web/MasterPage.master.cs
@@ -1,30 +1,13 @@
 ﻿using System;
-using System.Data;
-using System.Configuration;
-using System.Collections;
+using System.Collections.Generic;
 using System.Web;
-using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Web.UI.WebControls.WebParts;
-using System.Web.UI.HtmlControls;
 
 namespace ERP.Web
 {
     public partial class MasterPage : System.Web.UI.MasterPage
     {
-        public string _tabtitle = "";
-        public string TabTitle
-        {
-            get
-            {
-                return _tabtitle;
-            }
-            set
-            {
-                _tabtitle = value;
-            }
-        }
         protected void Page_Load(object sender, EventArgs e)
         {
 
