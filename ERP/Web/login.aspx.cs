3c5b42cfa2e38cc556d6938cf7c268708a9539e4
Donglin Song <songdl2014@foxmail.com>
2016-03-02 16:18:48 +0800

    login.aspx.cs，登录功能实现。调试通过，并添加调试后产生的文件。添加用户（admin,admin）。Web.config中loginUrl值Login.aspx改为login.aspx。

--- ERP/Web/login.aspx.cs
+++ ERP/Web/login.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -11,12 +12,29 @@ namespace ERP.Web
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            //Membership.CreateUser("admin", "admin");
         }
 
         protected void ImageButtonLogin_Click(object sender, ImageClickEventArgs e)
         {
+            try
+            {
+                if (Membership.ValidateUser(this.TextBoxUserName.Text, this.TextBoxPassword.Text))
+                {
+                    FormsAuthentication.SetAuthCookie(this.TextBoxUserName.Text, false);
 
+                    Response.Redirect(FormsAuthentication.DefaultUrl);
+                }
+                else
+                {
+                    this.LabelInfo.Text = "登录失败";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                this.LabelInfo.Text = ex.Message;
+            }
         }
     }
 }
--- ERP/Web/login.aspx.designer.cs
+++ ERP/Web/login.aspx.designer.cs
@@ -47,5 +47,14 @@ namespace ERP.Web {
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
         protected global::System.Web.UI.WebControls.ImageButton ImageButtonLogin;
+        
+        /// <summary>
+        /// LabelInfo 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelInfo;
     }
 }
